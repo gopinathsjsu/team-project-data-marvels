332a84d0dd528f592cab01a80445bf5a3c09c182
Checkout <priyankamath16@gmail.com>
2022-04-13 18:02:58 -0700

    Static login and signup pages changes

--- frontend/src/pages/auth/component/login.js
+++ frontend/src/pages/auth/component/login.js
@@ -1,30 +1,121 @@
 import React from 'react';
-import { Button, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Container, Button, TextField, Typography, Box, Grid, Link, 
+        Card, InputAdornment, IconButton, CssBaseline } from '@mui/material';
+import {Visibility, VisibilityOff} from '@mui/icons-material/';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {NavLink} from 'react-router-dom';
 
+const theme = createTheme();
+
 export default function Login() {
+  const [val, setVal] = useState({
+    email: "",
+    password:"",
+    showPassword: false,
+  })
+
+  function onChange(newval, id) {
+    let temp = { ...val }
+    temp[id] = newval
+    setVal(temp)
+  }
+
+  function login(e) {
+    e.preventDefault();
+    let data = {
+        ...val
+    }
+    console.log(data);
+  }
+
+  const handleClickShowPassword = () => {
+    setVal({
+      ...val,
+      showPassword: !val.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+  };
+
+
   return (
-    <>
-      <TextField
-        id="outlined-basic"
-        label="Username"
-        variant="outlined"
-        required={true}
-        placeholder="Enter your username"
-      />
-      <TextField
-        id="outlined-password-input"
-        label="Password"
-        type="password"
-        variant="outlined"
-        placeholder="Enter your password"
-        required={true}
-      />
-      <Button variant="contained">Login</Button>
-      <Typography variant="caption" display="block" gutterBottom>
-        New User ?
-      </Typography>
-      <NavLink to="/signup">SignUp</NavLink>
-    </>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth={false} sx={{ bgcolor: '#E7E5EA', height: '100vh', 
+                                                          display: 'flex', flexDirection: 'row',
+                                                          justifyContent: 'center'}}>
+        <CssBaseline />
+        <Card
+          sx={{
+            maxWidth: 600,
+            height: 'fit-content',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            paddingTop: 6,
+            paddingBottom: 6,
+            paddingLeft: 15,
+            paddingRight: 15,
+            marginTop: 10
+        }}>
+          <Typography variant="h3"> Sign in</Typography>
+          <Box component="form" noValidate sx={{ mt: 3, display: 'flex',
+                                                flexDirection: 'column',
+                                                alignItems: 'center', justifyContent: 'center' }} 
+                                                onSubmit={(e) => login(e)} id="login">
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+              <TextField 
+                name="email"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                autoFocus
+                autoComplete="email"
+                value={val.email}
+                onChange={(e) => onChange(e.target.value, "email")}/>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField 
+                  name="password"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  autoFocus
+                  autoComplete="new-password"
+                  type={val.showPassword ? 'text' : 'password'}
+                  value={val.password}
+                  onChange={(e) => onChange(e.target.value, "password")}
+                  endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {val.showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }/>
+              </Grid>
+            </Grid>
+            <Button variant="contained" type="submit"
+                    width= "auto" sx={{ mt: 3, mb: 2 }}
+                    form="login">SIGN IN</Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <NavLink to="/signup"> New User? Sign Up</NavLink>
+              </Grid>
+            </Grid>
+            
+          </Box>
+        </Card>
+      </Container>
+    </ThemeProvider>
   )
 }
--- frontend/src/pages/auth/component/signup.js
+++ frontend/src/pages/auth/component/signup.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Container, Typography, Box, Grid, Link, TextField, Button, CssBaseline } from '@mui/material/';
+import { Container, Typography, Box, Grid, Link, 
+    TextField, Button, CssBaseline, Card,
+    InputAdornment, IconButton } from '@mui/material/';
+import {Visibility, VisibilityOff} from '@mui/icons-material/';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Elements from '../../../common/Element';
 import { NavLink } from 'react-router-dom';
@@ -10,16 +13,28 @@ const theme = createTheme();
 export default function SignUp() {
 
     const [val, setVal] = useState({
-        name: "",
+        firstName: "",
+        lastName:"",
         email: "",
-        phoneNumber: ""
+        password:"",
+        showPassword: false,
     })
 
-    function onchange(newval, id) {
+    function onChange(newval, id) {
         let temp = { ...val }
         temp[id] = newval
         setVal(temp)
     }
+    const handleClickShowPassword = () => {
+        setVal({
+          ...val,
+          showPassword: !val.showPassword,
+        });
+      };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
     function register(e) {
         e.preventDefault();
@@ -31,86 +46,112 @@ export default function SignUp() {
 
     return (
         <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
+            <Container component="main" maxWidth={false} sx={{ bgcolor: '#E7E5EA', height: '100vh', 
+                                                                display: 'flex', flexDirection: 'row',
+                                                                justifyContent: 'center'}}>
                 <CssBaseline />
-                <Box
+                <Card
                     sx={{
-                        marginTop: 8,
+                        maxWidth: 600,
+                        height: 'fit-content',
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
+                        paddingTop: 6,
+                        paddingBottom: 6,
+                        paddingLeft: 15,
+                        paddingRight: 15,
+                        marginTop: 10
                     }}
                 >
-                    <Typography component="h1" variant="h5">
+                    <Typography variant="h3">
                         Sign up
                     </Typography>
-                    <Box component="form" noValidate sx={{ mt: 3 }} onSubmit={(e) => register(e)} id="register">
+                    <Box component="form" noValidate sx={{ mt: 3, display: 'flex',
+                                                            flexDirection: 'column',
+                                                            alignItems: 'center', justifyContent: 'center' }} 
+                                                            onSubmit={(e) => register(e)} id="register">
                         <Grid container spacing={2}>
-                            <Grid item xs={12} >
-                                <Elements
-                                    formField={[
-                                        {
-                                            type: "text",
-                                            id: "name",
-                                            required: true,
-                                            label: "Full Name",
-                                            value: val.name,
-                                            onchange: onchange,
-                                        },
-                                        {
-                                            type: "text",
-                                            id: "email",
-                                            required: true,
-                                            label: "Email Address",
-                                            value: val.email,
-                                            onchange: onchange,
-                                        }
-                                    ]}
-                                />
-                            </Grid>
-                            {/* <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                    value={val.email}
-                                    onChange={(e) => onchange(e.target.value, "email")}
-                                />
-                            </Grid> */}
                             <Grid item xs={12}>
-                                <TextField
-                                    name="Phone Number"
-                                    required
-                                    fullWidth
-                                    id="phoneNumber"
-                                    label="Phone Number"
-                                    autoFocus
-                                    value={val.phoneNumber}
-                                    onChange={(e) => onchange(e.target.value, "phoneNumber")}
-                                />
+                                <TextField 
+                                name="firstName"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                autoFocus
+                                autoComplete="given-name"
+                                value={val.firstName}
+                                onChange={(e) => onChange(e.target.value, "firstName")}/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField 
+                                name="lastName"
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                autoFocus
+                                autoComplete="family-name"
+                                value={val.lastName}
+                                onChange={(e) => onChange(e.target.value, "lastName")}/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField 
+                                name="email"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                autoFocus
+                                autoComplete="email"
+                                value={val.email}
+                                onChange={(e) => onChange(e.target.value, "email")}/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField 
+                                name="password"
+                                required
+                                fullWidth
+                                id="password"
+                                label="Password"
+                                autoFocus
+                                autoComplete="new-password"
+                                type={val.showPassword ? 'text' : 'password'}
+                                value={val.password}
+                                onChange={(e) => onChange(e.target.value, "password")}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                      <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                      >
+                                        {val.showPassword ? <VisibilityOff /> : <Visibility />}
+                                      </IconButton>
+                                    </InputAdornment>
+                                  }/>
                             </Grid>
                         </Grid>
                         <Button
                             type="submit"
-                            fullWidth
+                            width= "auto"
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
                             form="register"
                         >
-                            Sign Up
+                            SIGN UP
                         </Button>
-                        <Grid container justifyContent="flex-end">
+                        <Grid container justifyContent="center">
                             <Grid item>
-                                <NavLink to="/" variant="body2">
+                                <NavLink to="/login">
                                     Already have an account? Sign in
                                 </NavLink>
                             </Grid>
                         </Grid>
                     </Box>
-                </Box>
+                </Card>
             </Container>
         </ThemeProvider>
     );
