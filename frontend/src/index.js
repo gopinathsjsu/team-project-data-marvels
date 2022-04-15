177b5ebb37cadc60b132cc53a6bf57f3cf721518
Yash-Kamtekar <yash.kamtekar@sjsu.edu>
2022-04-15 13:35:00 -0700

    Bootstrap added and material ui removed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WebPage from "./routes/Router";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
