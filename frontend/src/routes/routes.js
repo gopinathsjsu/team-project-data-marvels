f64eb19992e90597e43fb07097c75f7cffcec584
Checkout <priyankamath16@gmail.com>
2022-04-27 14:10:20 -0700

    registered routes

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function getRoutes() {
     const Hotel = React.lazy(() => import('../pages/hotels/index'))
+    const Cart = React.lazy(() => import('../pages/cart/index'))
+    const BookingMgmt = React.lazy(() => import('../pages/bookingmgmt/index'))
+    const Reservations = React.lazy(() => import('../pages/reservations/index'))
 
     let routes = [
-        { path: '/app/hotel', name: 'Hotel', component: Hotel }
+        { path: '/app/hotel', name: 'Hotel', component: Hotel },
+        { path: '/app/cart', name: 'Cart', component: Cart },
+        { path: '/app/bookingmgmt', name: 'Booking Management', component: BookingMgmt },
+        { path: '/app/reservations', name: 'Reservations', component: Reservations },
     ]
 
     return routes
