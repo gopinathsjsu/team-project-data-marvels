a5a21bc8ddd3e75520a500c914b7c2bec6476bec
Checkout <priyankamath16@gmail.com>
2022-04-27 12:13:19 -0700

    used reusuable select in manage booking component

--- frontend/src/pages/hotels/index.js
+++ frontend/src/pages/hotels/index.js
@@ -31,7 +31,7 @@ function Hotels() {
                     </div>
                 </div>
                 <ul className='list-group list-group-flush mt-2'>
-                    <li className='list-group-item d-flex flex-row justify-content-between mt-3' style={{ backgroundColor: '#E7E5EA', border: '1px solid rgba(0,0,0,.125)', borderWidth: '0 0 1px' }}>
+                    <li className='list-group-item d-flex flex-row justify-content-between mt-3 border-bottom-1' style={{ backgroundColor: '#E7E5EA' }}>
                         <div className='col-4 d-flex flex-column justify-content-around'>
                             <h6>Hotel Name</h6>
                             <p className='mb-0'>Address</p>
@@ -41,7 +41,7 @@ function Hotels() {
                             <p>Rating</p>
                         </div>
                     </li>
-                    <li className='list-group-item d-flex flex-row justify-content-between mt-3' style={{ backgroundColor: '#E7E5EA', border: '1px solid rgba(0,0,0,.125)', borderWidth: '0 0 1px' }}>
+                    <li className='list-group-item d-flex flex-row justify-content-between mt-3 border-bottom-1' style={{ backgroundColor: '#E7E5EA' }}>
                         <div className='col-4 d-flex flex-column justify-content-around'>
                             <h6>Hotel Name</h6>
                             <p className='mb-0'>Address</p>
