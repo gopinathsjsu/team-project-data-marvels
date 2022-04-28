ceb84da600dc504df82588e21aca9582002dad26
Yash-Kamtekar <yash.kamtekar@sjsu.edu>
2022-04-28 16:34:00 -0700

    Nav link from routing component replaced

--- frontend/src/common/component/Header.js
+++ frontend/src/common/component/Header.js
@@ -1,75 +1,37 @@
 import React, { useState } from 'react';
 
+import { NavLink } from 'react-router-dom';
+
 import { connect } from 'react-redux';
 
 import { Navbar, Nav } from 'react-bootstrap';
-import { Button, Modal } from '../index';
-import Login from '../../pages/auth/component/login';
-import Signup from '../../pages/auth/component/signup';
+import { Button } from '../index';
 
-function Header(props) {
-    console.log(props.profile.data);
-    const [modalData, setModalData] = useState({ title: '', body: '', footer: '' })
-    const modalId = 'auth-modal';
+function Header() {
 
     return (
         <>
             <Navbar bg="dark" variant="dark" className='d-flex flow-row justify-content-between'>
                 <Nav className='d-flex flex-row ms-4'>
-                    <Navbar.Brand href='/home'>Marvel Hotels</Navbar.Brand>
-                    <Nav.Link href='/home'>Home</Nav.Link>
-                    <Nav.Link href='/app/hotels'>Hotels</Nav.Link>
-                    <Nav.Link href='/app/manageBooking'>Manage Reservations</Nav.Link>
+                    <NavLink to='/app'>Marvel Hotel</NavLink>
+                    <NavLink to='/app'>Home</NavLink>
+                    <NavLink to='/app/hotels'>Hotels</NavLink>
+                    <NavLink to='/app/manageBooking'>Manage Reservations</NavLink>
                 </Nav>
                 <Nav className='d-flex flex-row me-4'>
-                    <Nav.Link href='/app/reservations'>My Reservations</Nav.Link>
-                    <Nav.Link href='/app/cart'>Cart</Nav.Link>
+                    <NavLink to='/app/reservations'>My Reservations</NavLink>
+                    <NavLink to='/app/cart'>Cart</NavLink>
 
-                    {
-                        props.profile.data !== undefined ? <Button
-                            text='Logout'
-                            variant='header-link'
-                            onClick={() => {
-                                localStorage.clear()
-                                window.location.href = '/'
-                            }}
-                        /> : <Button
-                            text='Login / Sign up'
-                            variant='header-link'
-                            target={modalId}
-                            onClick={() => {
-                                let temp = {
-                                    title: '',
-                                    body: (
-                                        <Login modalId={modalId} setModalData={setModalData} />
-                                    ),
-                                }
-                                setModalData(temp)
-                            }}
-                        />
-                    }
-                    {/* <Button
-                        text='Sign up'
+                    <Button
+                        text='Logout'
                         variant='header-link'
-                        target={modalId}
                         onClick={() => {
-                            let temp = {
-                                title: '',
-                                body: (
-                                    <Signup modalId={modalId} setModalData={setModalData} />
-                                ),
-                            }
-                            setModalData(temp)
+                            localStorage.clear()
+                            window.location.href = '/'
                         }}
-                    /> */}
+                    />
                 </Nav>
             </Navbar>
-            <Modal
-                modalId={modalId}
-                title={modalData.title}
-                body={modalData.body}
-                footer={modalData.footer}
-            />
         </>
     )
 }
