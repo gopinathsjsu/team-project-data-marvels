3e8a5858f5d1426b258e6493130926d00c6c2d20
Checkout <priyankamath16@gmail.com>
2022-04-28 10:30:40 -0700

    updated link refs in header

--- frontend/src/common/component/Header.js
+++ frontend/src/common/component/Header.js
@@ -14,12 +14,12 @@ function Header() {
                 <Nav className='d-flex flex-row ms-4'>
                     <Navbar.Brand href='/home'>Marvel Hotels</Navbar.Brand>
                     <Nav.Link href='/home'>Home</Nav.Link>
-                    <Nav.Link href='/hotels'>Hotels</Nav.Link>
-                    <Nav.Link href='/manageBooking'>Manage Reservations</Nav.Link>
+                    <Nav.Link href='/app/hotels'>Hotels</Nav.Link>
+                    <Nav.Link href='/app/manageBooking'>Manage Reservations</Nav.Link>
                 </Nav>
                 <Nav className='d-flex flex-row me-4'>
-                    <Nav.Link href='/reservations'>My Reservations</Nav.Link>
-                    <Nav.Link href='/cart'>Cart</Nav.Link>
+                    <Nav.Link href='/app/reservations'>My Reservations</Nav.Link>
+                    <Nav.Link href='/app/cart'>Cart</Nav.Link>
                     <Button
                         text='Login'
                         variant='header-link'
