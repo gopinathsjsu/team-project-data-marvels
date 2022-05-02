2e72ccf3b4ce940be228c6aafd141df72292fa73
Tharun Mukka <tharunmukka1@gmail.com>
2022-05-02 12:11:22 -0700

    added password wrong logic, hotel controller

--- backend/src/main/java/com/example/backend/Repository/UsersRepository.java
+++ backend/src/main/java/com/example/backend/Repository/UsersRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
     Users findByUsernameAndPass(String username, String pass);
+    Users findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
